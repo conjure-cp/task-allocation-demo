d30f0411cf3139889b8fe13af5aa85f7cc0b5291
YuxinZhang214 <72717144+YuxinZhang214@users.noreply.github.com>
2023-11-09 20:07:44 +0000

    Horizontal Contributors list & reusable banner with config file

---
+++ config/banner.js
@@ -0,0 +1,13 @@
+const bannerConfig = {
+    logo        :'conjure-cp',
+    title       : "Task allocation",
+    author      : "williamburns",
+    description : "This project is created as a dissertation project at the University of St Andrews for optimal task allocation.",
+    repo_url    :'conjure-cp/task-allocation-demo',
+    github      : `https://github.com/conjure-cp/task-allocation-demo`,
+    license     :'Mozilla Public License 2.0'
+};
+
+bannerConfig.author_github = `https://github.com/${bannerConfig.author}`;
+  
+export default bannerConfig;
