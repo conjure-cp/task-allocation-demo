62d83359d9c15f14af6b647a23bd4d9fbb2231a1
YuxinZhang214 <72717144+YuxinZhang214@users.noreply.github.com>
2023-11-10 21:32:20 +0000

    Fixed Author name & README

--- config/banner.js
+++ config/banner.js
@@ -1,13 +1,12 @@
 const bannerConfig = {
-    logo        :'conjure-cp',
-    title       : "Task allocation",
-    author      : "williamburns",
-    description : "This project is created as a dissertation project at the University of St Andrews for optimal task allocation.",
-    repo_url    :'conjure-cp/task-allocation-demo',
-    github      : `https://github.com/conjure-cp/task-allocation-demo`,
-    license     :'Mozilla Public License 2.0'
+    logo          : 'conjure-cp',
+    title         : "Task allocation",
+    author        : "William Burns",
+    author_github : "https://github.com/williamburns",
+    description   : "This project is created as a dissertation project at the University of St Andrews for optimal task allocation.",
+    repo_url      : 'conjure-cp/task-allocation-demo',
+    github        : `https://github.com/conjure-cp/task-allocation-demo`,
+    license       : 'Mozilla Public License 2.0'
 };
-
-bannerConfig.author_github = `https://github.com/${bannerConfig.author}`;
   
 export default bannerConfig;
