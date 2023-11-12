397ad5abb0409a5a686513765ceb5292988de50b
Özgür Akgün <ozgurakgun@gmail.com>
2023-11-12 11:25:55 +0000

    remove node_modules first and skip non html files

--- scripts/remove-imports.js
+++ scripts/remove-imports.js
@@ -1,18 +1,20 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
+const path = require('path');
 
 process.argv.forEach((filename, index) => {
     fs.readFile(filename, 'utf8', (err, data) => {
-        if (err) throw err;
-
-        const $ = cheerio.load(data);
-        $('link').attr('href', 'REMOVED');
-        $('script').attr('src', 'REMOVED');
-
-        fs.writeFile(filename, $.html(), (err) => {
+        if (path.extname(filename) == ".html") {
             if (err) throw err;
-            console.log(`File has been modified: ${filename}`);
-        });
-    });
 
+            const $ = cheerio.load(data);
+            $('link').attr('href', 'REMOVED');
+            $('script').attr('src', 'REMOVED');
+
+            fs.writeFile(filename, $.html(), (err) => {
+                if (err) throw err;
+                console.log(`File has been modified: ${filename}`);
+            });
+        }
+    });
 });
