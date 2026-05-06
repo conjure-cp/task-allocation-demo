61fab8705c3f0808b55f1cdcb109035d7893ffbc
ozgurakgun <ozgurakgun@gmail.com>
2026-05-06 19:47:47 +0000

    Deploying to gh-pages from @ conjure-cp/task-allocation-demo@6a657b35bd621db98fc49dc3b95216d0c44c9e7b 🚀

--- _next/static/chunks/0tj8-9.42w6~x.js
+++ _next/static/chunks/0tj8-9.42w6~x.js
@@ -5,6 +5,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/0rq6l3s~787sf.js",
   "static/chunks/09g8baud4rz1u.js",
   "static/chunks/0j3yzhutgb5az.js",
-  "static/chunks/0rj.l3by9c-_v.css",
-  "static/chunks/turbopack-0f9t~mryk2~oc.js"
+  "static/chunks/0jq-3.7pzxdu0.css",
+  "static/chunks/turbopack-06npz9aqozxke.js"
 ])
